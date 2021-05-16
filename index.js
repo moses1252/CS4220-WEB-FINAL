409c74d255199935c63b1a872eb5cd7a11cf0d69
Anthony Viramontes <47967342+moses1252@users.noreply.github.com>
2021-05-16 15:44:59 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,58 +2,37 @@ const superagent = require('superagent');
 
 const config = require('./config.json');
 
-//const base = 'https://rickandmortyapi.com/api/';
-// const card = './CharacterCard';
-// const app = require('../npm-app/app.js');
-// const search = async (keyword) => {
-//     try {
-//         const url = `${config.url}/character/${keyword}`;
-//         console.log(url);
-//         const response = await superagent.get(url);
-//         return response.body;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
-const nextPage = async (page = 1, type = 'chara') => {
+const nextPage = async (page = 1, keyword) => {
     try {
-        if (type === 'chara') {
+        if (keyword === 'character') {
             page++;
             const getCharacter = `${config.url}/character/?page=${page}`;
-            const response = await superagent.get(getCharacter); //[character] -- prints out 'code' 
+            const response = await superagent.get(getCharacter);
 
             return response.body;
         }
-        // if (type === 'loca') {
-        //     page++;
-        //     const getCharacter = `${base}location?page=${page}`;
-        //     const response = await superagent.get(getCharacter); //[character] -- prints out 'code' 
+        if (keyword === 'location') {
+            page++;
+            const getCharacter = `${config.url}/location?page=${page}`;
+            const response = await superagent.get(getCharacter);
 
-        //     return response.body;
-        // }
-        // if (type === 'epi') {
-        //     page++;
-        //     const getCharacter = `${base}episode?page=${page}`;
-        //     const response = await superagent.get(getCharacter); //[character] -- prints out 'code' 
-
-        //     return response.body;
-        // }
-
+            return response.body;
+        }
     } catch (error) {
         return error;
     }
 };
 
 //recieve data from API 
-const getAll = async (type = 'chara') => {
+const getAll = async (keyword) => {
     //characters
-    if (type === 'chara') {
+    if (keyword === 'character') {
         let pages;
-        let hold = []
+        let hold = [];
         try {
-            for (i = 0; i < 3; i++) {
+            for (i = 0; i < 5; i++) {
                 for (j = 0; j < 20; j++) {
-                    pages = await nextPage(i, 'chara');
+                    pages = await nextPage(i, keyword);
                     hold.push(pages["results"][j]);
                 }
             }
@@ -63,13 +42,13 @@ const getAll = async (type = 'chara') => {
         return hold;
     }
     //locations
-    if (type === 'loca') {
+    if (keyword === 'location') {
         let pages;
-        let hold = []
+        let hold = [];
         try {
-            for (i = 0; i < 3; i++) {
+            for (i = 0; i < 5; i++) {
                 for (j = 0; j < 20; j++) {
-                    pages = await nextPage(i, 'loca');
+                    pages = await nextPage(i, keyword);
                     hold.push(pages["results"][j]);
                 }
             }
@@ -78,251 +57,51 @@ const getAll = async (type = 'chara') => {
         }
         return hold;
     }
-    if (type === 'epi') {
-        let pages;
-        let hold = []
-        try {
-            for (i = 0; i < 2; i++) {
-                for (j = 0; j < 20; j++) {
-                    pages = await nextPage(i, 'epi');
-                    hold.push(pages["results"][j]);
-                }
-            }
-        } catch (error) {
-            return error;
-        }
-        return hold;
-    }
-
-
 }
 
-const getAllCharacters = async () => {
-    let pages;
-    let hold = []
-    try {
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 20; j++) {
-                pages = await nextPage(i, 'chara');
-                hold.push(pages["results"][j]);
-            }
-        }
-    } catch (error) {
-        return error;
-    }
-    return hold;
-};
-
-// const getAllLocations = async () => {
-//     let pages;
-//     let hold = []
-//     try {
-//         for (i = 0; i < 3; i++) {
-//             for (j = 0; j < 20; j++) {
-//                 pages = await nextPage(i, 'loca');
-//                 hold.push(pages["results"][j]);
-//             }
-//         }
-//     } catch (error) {
-//         return error;
-//     }
-//     return hold;
-// }
-
-// const getAllEpisodes = async () => {
-//     let pages;
-//     let hold = []
-//     try {
-//         for (i = 0; i < 2; i++) {
-//             for (j = 0; j < 20; j++) {
-//                 pages = await nextPage(i, 'epi');
-//                 hold.push(pages["results"][j]);
-//             }
-//         }
-//     } catch (error) {
-//         return error;
-//     }
-//     return hold;
-// }
-
-const search = async (keyword) => {
-    try {
-        const url = `${config.url}/character/${keyword} `;
-        console.log(url);
-        const response = await superagent.get(url);
-        return response.body;
-    } catch (error) {
-        console.log(error);
-    }
-};
- 
-const findAll = async (keyword, type = 'chara') => {
-    if (type === 'chara') {
+const findAll = async (keyword, type = 'character') => {
+    if (type === 'character') {
+        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~');
         try {
-            for (i = 0; i <33; i++) {
-                let pages = await nextPage(i, 'chara');
+            for (i = 0; i < 5; i++) {
+                let pages = await nextPage(i, 'character');
                 for (j = 0; j < 20; j++) {
-                    //for (r = 0; r < ch.length; r++) {
-                        let pageName = JSON.stringify(pages["results"][j].name);
-                        pageName = pageName.toLowerCase();
-                        let check = keyword.toLowerCase();
-
-                        if (pageName.includes(check)) {
-                            console.log("ID: " + pages["results"][j].id +
+                    let pageName = JSON.stringify(pages["results"][j].name);
+                    if (pageName.includes(keyword)) {
+                        console.log("ID: " + pages["results"][j].id +
                             "\nName: " + pages["results"][j].name +
-                            "\nStatus: " + pages["results"][j].status + 
+                            "\nStatus: " + pages["results"][j].status +
                             "\nSpecies: " + pages["results"][j].species);
-                            console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~');
-                            // pages["results"][j].episode.forEach(element => {
-                            //     console.log("Characters: " + element);
-                            // });
-                        }
-
-                    //}------------------------------------------
+                        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~');
+                    }
                 }
             }
-            console.log("this async function is done running");
         } catch (error) {
             return error;
         }
     }
-    // if (type === 'loca') {
-    //     try {
-    //         for (i = 0; i < 5; i++) {
-    //             let pages = await nextPage(i, 'loca');
-    //             for (j = 0; j < 20; j++) {
-    //                 for (r = 0; r < ch.length; r++) {
-    //                     if (ch[r] === JSON.stringify(pages["results"][j].name)) {
-    //                         console.log("ID: " + pages["results"][j].id + 
-    //                         "\nName: " + pages["results"][j].name +
-    //                         "\nType: " + pages["results"][j].type +
-    //                         "\nDimenstion: " + pages["results"][j].type);
-    //                     }
-    //                 }
-
-    //             }
-    //         }
-    //         //console.log("this async function is done running");
-    //     } catch (error) {
-    //         return error;
-    //     }
-    // }
-    // if (type === 'epi') {
-    //     try {
-    //         for (i = 0; i < 3; i++) {
-    //             let pages = await nextPage(i, 'epi');
-    //             for (j = 0; j < 20; j++) {
-    //                 for (r = 0; r < ch.length; r++) {
-    //                     if (ch[r] === JSON.stringify(pages["results"][j].name)) {
-    //                         console.log("ID: " + pages["results"][j].id + 
-    //                         "\nName: " + pages["results"][j].name +
-    //                         "\nAir Date: " + pages["results"][j].air_date +
-    //                         "\nEpisode: " + pages["results"][j].episode);
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //         //console.log("this async function is done running");
-    //     } catch (error) {
-    //         return error;
-    //     }
-    // }
+    if (type === 'location') {
+        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~');
+        try {
+            for (i = 0; i < 5; i++) {
+                let pages = await nextPage(i, 'location');
+                for (j = 0; j < 5; j++) {
+                    let pageName = JSON.stringify(pages["results"][j].name);
+                    if (pageName.includes(keyword)) {
+                        console.log("ID: " + pages["results"][j].id +
+                            "\nName: " + pages["results"][j].name);
+                        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~');
+                    }
+                }
+            }
+        } catch (error) {
+            return error;
+        }
+    }
 }
-
-// const findCharacter = async (ch = ["Morty’s Girlfriend"]) => { // the parameter should be a character name
-//     try {
-//         for (i = 0; i < 33; i++) {
-//             let pages = await nextPage(i, 'chara');
-//             for (j = 0; j < 20; j++) {
-//                 for (r = 0; r < ch.length; r++) {
-//                     if (ch[r] === JSON.stringify(pages["results"][j].name)) {
-//                         console.log(pages["results"][j]);
-
-//                     }
-//                 }
-
-//             }
-//         }
-//         console.log("this async function is done running");
-//     } catch (error) {
-//         return error;
-//     }
-// };
-
-// const findLocation = async (ch = ["Earth (C-137)"]) => { // the parameter should be a character name
-//     try {
-//         for (i = 0; i < 5; i++) {
-//             let pages = await nextPage(i, 'loca');
-//             for (j = 0; j < 20; j++) {
-//                 for (r = 0; r < ch.length; r++) {
-//                     if (ch[r] === JSON.stringify(pages["results"][j].name)) {
-//                         console.log(pages["results"][j]);
-
-//                     }
-//                 }
-
-//             }
-//         }
-//         console.log("this async function is done running");
-//     } catch (error) {
-//         return error;
-//     }
-// };
-
-// const findEpisode = async (ch = ["The Wedding Squanchers"]) => { // the parameter should be a character name
-//     try {
-//         for (i = 0; i < 3; i++) {
-//             let pages = await nextPage(i, 'epi');
-//             for (j = 0; j < 20; j++) {
-//                 for (r = 0; r < ch.length; r++) {
-//                     if (ch[r] === JSON.stringify(pages["results"][j].name)) {
-//                         console.log(pages["results"][j]);
-//                     }
-//                 }
-
-//             }
-//         }
-//         console.log("this async function is done running");
-//     } catch (error) {
-//         return error;
-//     }
-// };
-
-// //this is for my testing methods
-// const printCharacters = async () => {
-//     let pages;
-//     try {
-//         for (i = 0; i < 1; i++) {
-//             pages = await nextPage(i, 'chara');
-//             for (j = 0; j < 20; j++) {
-//                 console.log(pages["results"][j].name);
-//                 //result.push(pages["results"][j].name);
-//             }
-//         }
-//     } catch (error) {
-//         return error;
-//     }
-// };
-
-// const printLocations = async () => {
-//     let pages;
-//     try {
-//         for (i = 0; i < 1; i++) {
-//             pages = await nextPage(i, 'loca');
-//             for (j = 0; j < 20; j++) {
-//                 console.log(pages["results"][j].name);
-//             }
-//         }
-//     } catch (error) {
-//         return error;
-//     }
-// };
-
-
 
 module.exports = {
     findAll,
     nextPage,
-    getAll,
-    search
+    getAll
 };
