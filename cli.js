bdedccb2b45d488dd90ad1d7940739874f9d4148
Anthony Viramontes <47967342+moses1252@users.noreply.github.com>
2021-05-16 15:45:16 -0700

    Add files via upload

--- cli.js
+++ cli.js
@@ -6,18 +6,16 @@ yargs
     // add the 'search' command
     .command({
         command: 'search',
-        desc: 'You will have access to all characters, locations, and episodes from the show Rick and Morty Show',
+        desc: 'You will have access to all characters and locations from the show Rick and Morty Show',
         builder: (yargs) => {
             // add the search option to the search command
             return yargs.options('d', {
                 alias: 'document',
-                describe: 'You can choose from a list of characters, locations, or episode and it will display information about them.'
+                describe: 'You can choose from a list of characters or locations and it will display information about them.'
             });
         },
         handler: (argv) => {
             app.search(argv.document);
-            //app.search(argv.shuffle);
-            //console.log(argv);
         }
     })
     // add a help menu to assist the user in understanding our CLI
